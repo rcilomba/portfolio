0eed9fb013cb772b82eb458d1f59748f5e277e19
rcilomba <32643822+rcilomba@users.noreply.github.com>
2020-06-29 01:56:50 +0200

    Add files via upload
    padding text content, to middle
    anchor to top page

---
+++ script.js
@@ -0,0 +1,14 @@
+//window.alert("Meni sayd he would help but he didnt't :(");
+/*let name ='Ramma';
+console.log(name);
+
+let firstName = "Ramz";
+let FirstName;
+console.log(firstName + FirstName);*/
+
+function myFunction(p1, p2){
+    return p1 + p2;
+
+}
+
+document.getElementById('center').innerHTML= Date()
